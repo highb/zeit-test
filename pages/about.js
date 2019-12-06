62e77196e1c4906d948b0d3bb092b6b3d6c490b9
Brandon High <brandon.high@puppet.com>
2019-12-06 13:19:56 -0800

    Finish "Using shared components"

--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,26 @@
-import Header from '../components/Header';
+// import Header from '../components/MyLayout';
+// import Layout from '../components/MyLayout';
+
+// export default function About() {
+//   return (
+//     <Layout>
+//       <p>This is the about page</p>
+//     </Layout>
+//   );
+// }
+
+// Higher-order
+// import withLayout from '../components/MyLayoutHigherOrder';
+
+// const Page = () => <p>This is the about page</p>;
+
+// export default withLayout(Page);
+
+// Prop passed in
+import Layout from '../components/MyLayoutProp';
+
+const aboutPageContent = <p>This is the about page</p>;
 
 export default function About() {
-  return (
-    <div>
-      <Header />
-      <p>This is the about page</p>
-    </div>
-  );
+  return <Layout content={aboutPageContent} />;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,27 @@
 // This is the Link API
-import Header from '../components/Header';
+// import Header from '../components/Header';
+// import Layout from '../components/MyLayout';
 
-const Index = () => (
-  <div>
-    <Header />
-    <p>Hello Next.js</p>
-  </div>
-);
+// const Index = () => (
+//   <Layout>
+//     <p>Hello Next.js</p>
+//   </Layout>
+// );
 
-export default Index;
+// export default Index;
+
+// Higher-order component
+// import withLayout from '../components/MyLayoutHigherOrder';
+
+// const Page = () => <p>Hello Next.js</p>;
+
+// export default withLayout(Page);
+
+// Property passed to component
+import Layout from '../components/MyLayoutProp.js';
+
+const indexPageContent = <p>Hello Next.js</p>;
+
+export default function Index() {
+  return <Layout content={indexPageContent} />;
+}
