d9d7cd4409dc668fcea495153a38c08ba6ac5cdb
Brandon High <brandon.high@puppet.com>
2019-12-06 13:38:06 -0800

    Finshed "Create dynamic pages"

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,27 @@
-// This is the Link API
-// import Header from '../components/Header';
-// import Layout from '../components/MyLayout';
+import Layout from '../components/MyLayout';
+import Link from 'next/link';
 
-// const Index = () => (
-//   <Layout>
-//     <p>Hello Next.js</p>
-//   </Layout>
-// );
+const PostLink = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
+export default function Blog() {
+  return (
+    <Layout>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink title="Hello Next.js" />
+        <PostLink title="Learn Next.js is awesome" />
+        <PostLink title="Deploy apps with Zeit" />
+      </ul>
+    </Layout>
+  );
+}
 
-// export default Index;
+// Alternate ways of using components below:
 
 // Higher-order component
 // import withLayout from '../components/MyLayoutHigherOrder';
@@ -18,10 +31,10 @@
 // export default withLayout(Page);
 
 // Property passed to component
-import Layout from '../components/MyLayoutProp.js';
+// import Layout from '../components/MyLayoutProp.js';
 
-const indexPageContent = <p>Hello Next.js</p>;
+// const indexPageContent = <p>Hello Next.js</p>;
 
-export default function Index() {
-  return <Layout content={indexPageContent} />;
-}
+// export default function Index() {
+//   return <Layout content={indexPageContent} />;
+// }
