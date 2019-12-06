ebfabfad27f69dc8aa897c2ce04a7a67090d56a6
Brandon High <brandon.high@puppet.com>
2019-12-06 13:07:18 -0800

    Progress "Using shared components"

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 // This is the Link API
-import Link from 'next/link';
+import Header from '../components/Header';
 
 const Index = () => (
   <div>
-    <Link href="/about" title="About Page">
-      <a>About Page</a>
-    </Link>
+    <Header />
     <p>Hello Next.js</p>
   </div>
 );
